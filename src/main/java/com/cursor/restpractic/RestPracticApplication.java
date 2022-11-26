f6f4a4103889a20dc7e39c253e9079f53e79bae4
Yanita <yanita2823@gmail.com>
2022-11-26 15:13:50 +0200

    first commit

---
+++ src/main/java/com/cursor/restpractic/RestPracticApplication.java
@@ -0,0 +1,13 @@
+package com.cursor.restpractic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPracticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPracticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursor/restpractic/RestPracticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursor.restpractic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPracticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
